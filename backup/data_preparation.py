1582b7e6de5e6c0c1c7ace0e44ba35c23cfaeac5
Ladislav Ondris <ladislav.ondris@seznam.cz>
2018-03-30 13:15:16 +0200

    Working RNN up to 75 % accuracy.

---
+++ backup/data_preparation.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 24 12:32:24 2018
+
+@author: ladis
+"""
+
+import glob
+import numpy as np
+import pandas as pd
+import pickle
+import csv
+
+def create_feature_sets_and_labels(pattern, number_of_files, train_size_percent):
+    eng_rows, noneng_rows = get_rows(pattern, number_of_files)
+    
+    print(eng_rows.groupby(["lang"]).count())
+    print(noneng_rows.groupby(["lang"]).count())
+    
+    eng_sentences, noneng_sentences = get_only_sentences(eng_rows, noneng_rows)
+    np.random.shuffle(noneng_sentences)
+    eng_sentences, noneng_sentences = equalize_length_to_lower(eng_sentences, noneng_sentences)
+    
+    trainsize = int(train_size_percent / 100 * len(eng_sentences))
+    return eng_sentences[:trainsize], noneng_sentences[:trainsize], eng_sentences[trainsize:], noneng_sentences[trainsize:]
+    
+def get_rows(pattern, number_of_files=10):
+    files = glob.glob(pattern)
+    
+    english_rows = pd.DataFrame()
+    nonenglish_rows = pd.DataFrame()
+    
+    for index, file_name in enumerate(files):
+        if index == number_of_files:
+            break
+        
+        eng_rows, noneng_rows = extract_rows(file_name)
+        
+        english_rows = pd.concat([english_rows, eng_rows])
+        nonenglish_rows = pd.concat([nonenglish_rows, noneng_rows])
+    return english_rows, nonenglish_rows
+
+def equalize_length_to_lower(eng_sentences, noneng_sentences):
+    minlen = min(len(eng_sentences), len(noneng_sentences))
+    return eng_sentences[:minlen], noneng_sentences[:minlen]
+
+def get_only_sentences(eng_rows, noneng_rows):
+    english_sentences = eng_rows.iloc[:,2]
+    nonenglish_sentences = noneng_rows.iloc[:,2]
+    return np.array(english_sentences), np.array(nonenglish_sentences)
+           
+def extract_rows(file_name):
+    english_sentences = []
+    nonenglish_sentences = []
+    
+    table = pd.read_table(file_name, encoding="ISO-8859-1", delimiter="\t", quoting=csv.QUOTE_NONE, names=["index", "lang", "text"])
+    
+    noneng_langs = ["spa", "deu", "fra", "ita"]
+    
+    english_sentences = table[(table["lang"] == "eng") & (table["text"].notnull())]
+    nonenglish_sentences = table[(table["lang"].isin(noneng_langs)) & (table["text"].notnull())]
+    
+    return english_sentences, nonenglish_sentences
+    
+
+if __name__ == "__main__":
+    number_of_files = 437
+    train_size_percent = 80
+    
+    train_eng, train_noneng, test_eng, test_noneng = \
+        create_feature_sets_and_labels(r'C:\Users\ladis\OneDrive\Plocha\SentencesDataset\sentences*', \
+                                       number_of_files, train_size_percent)
+    print(len(train_eng))
+    print(len(train_noneng))
+    print(len(test_eng))
+    print(len(test_noneng))
+    with open('full_multilang_sentences.pickle', 'wb') as f:
+        pickle.dump([train_eng, train_noneng, test_eng, test_noneng], f)
+        print("done")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
---
+++ rnn.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 15:22:31 2018
+
+@author: ladis
+"""
+
+
+import tensorflow as tf
+import pickle
+import numpy as np
+import os
+
+from tensorflow.python.ops import rnn, rnn_cell
+
+
+hm_epochs = 5
+n_classes = 2
+batch_size = 64
+
+rnn_size = 512
+
+x = tf.placeholder("float", [None, 100, 255]) 
+y = tf.placeholder("float")
+def recurrent_neural_network(x):
+    layer = {'weights':tf.Variable(tf.random_normal([rnn_size, n_classes]), dtype=tf.float32),
+             'biases':tf.Variable(tf.random_normal([n_classes]), dtype=tf.float32)}
+    
+    X_seqs = tf.unstack(tf.transpose(x, perm=[1, 0, 2]))
+    lstm_cell = rnn_cell.BasicRNNCell(rnn_size, reuse=None)
+    outputs, states = rnn.static_rnn(lstm_cell, X_seqs, dtype=tf.float32)
+    
+    output = tf.add(tf.matmul(outputs[-1], layer['weights']), layer['biases'])
+    
+    return output;
+
+def train_neural_network(x, y, train_x, train_y, test_x, test_y):
+    prediction = recurrent_neural_network(x)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+    # learning_rate = 0.001
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        n_batches = int(len(train_x) / batch_size)
+        
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            
+            batches_x = [train_x[k:k+batch_size] for k in range(n_batches)]
+            batches_y = [train_y[k:k+batch_size] for k in range(n_batches)]
+            
+            for batch_x, batch_y in zip(batches_x, batches_y):
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+                epoch_loss += c
+            print('Epoch loss', epoch + 1, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+            
+        
+            correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+            print('Accuracy:', accuracy.eval({x:test_x, y:test_y}))
+        
+        
+def prepare_data(eng, noneng):
+    eng_x = sentencesToArrayOfASCII(eng)
+    noneng_x = sentencesToArrayOfASCII(noneng)
+    
+    eng_y = [[1, 0] for i in range(len(eng_x))]
+    noneng_y = [[0,1] for i in range(len(noneng_x))]
+    
+    all_x = np.concatenate((eng_x, noneng_x))
+    all_y = np.concatenate((eng_y, noneng_y))
+    
+    all_x, all_y = unison_shuffled_copies(all_x, all_y)
+   
+    return all_x, all_y
+      
+# takes array of sentences as input and returns those sentences represented in binary values 
+# every letter is reprezented as an array of length 255 (ASCII values) 
+# returns an array of shape ((len(sentences_as_strings),100, 255)     
+def sentencesToArrayOfASCII(sentences_as_strings):
+    sentences = np.ndarray((len(sentences_as_strings), 100, 255), dtype=np.float32)
+    for sentence_index, sentence in enumerate(sentences_as_strings):
+        sentences[sentence_index] = sentenceToBinary(sentence)
+    return sentences
+            
+
+# takes a single sentence as input and returns the sentence as an array of shape (100,255)
+# for every sentence returns 100 characters long array (either truncates the sentence of pads zeros)
+def sentenceToBinary(sentence_as_string):
+    sentence_chars = np.zeros((100,255), dtype=np.float32)
+    for index, char_as_number in enumerate(map(ord, list(sentence_as_string))):
+        if index >= 100:
+            break
+        sentence_chars[index][char_as_number] = 1
+    return np.array(sentence_chars)
+          
+
+# shuffles both arrays
+def unison_shuffled_copies(a, b, c):
+    assert len(a) == len(b) == len(c)
+    p = np.random.permutation(len(a))
+    return a[p], b[p], c[p]
+
+  
+filename = 'multilang_sentences_5.pickle'
+if os.path.getsize(filename) > 0:           
+    with open(filename, 'rb') as f:
+        train_eng, train_noneng, test_eng, test_noneng = pickle.load(f)
+        train_x, train_y = prepare_data(train_eng, train_noneng)
+        test_x, test_y = prepare_data(test_eng, test_noneng)
+        
+        
+    #print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
+    print("Training started.")
+    train_neural_network(x, y, train_x, train_y, test_x, test_y)
+else:
+    print("File is empty")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
